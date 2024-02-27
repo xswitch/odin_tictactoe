5c17a59626798c19d0199ed580bc2f6988ac0d19
xswitch <bivarmong@gmail.com>
2024-02-27 09:30:19 +0100

    Added ability to play rounds using x and y inputs

--- script.js
+++ script.js
@@ -4,41 +4,76 @@ const gameBoard = (function () {
      [0, 0, 0],
      [0, 0, 0]];
 
-    let currentMark = 'x'
-
-    function allEqual(array) {
-        return array.every((value => value == currentMark));
+    // Returns boardArray
+    function getBoard() {
+        return boardArray;
     }
 
-    function checkForWinner() {
+    function addMark(x, y, mark) {
+        if (boardArray[x][y] == 0) boardArray[x][y] = mark;
+    }
+
+    return {
+        getBoard,
+        addMark,
+    }
+})()
+
+const gameController = (function() {
+
+    let currentMark = 'x';
+
+    // Checks if all values in an array matches "currentMark"
+    function allEqual(array) {
+        return array.every((value => value == array[0] && value != 0));
+    }
+    
+    // Returns true if any check is true
+    function checkForWinner(array) {
         let winner = false;
 
         // Checks rows
-        boardArray.forEach(row => {
+        array.forEach(row => {
             console.log(row[0]);
             if (allEqual(row)) winner = true;
         })
 
         // Checks columns
-        boardArray[0].forEach((column, index) => {
-            if (allEqual([column, boardArray[1][index], boardArray[2][index]])) winner = true;
+        array[0].forEach((column, index) => {
+            if (allEqual([column, array[1][index], array[2][index]])) winner = true;
         })
 
         // Check diagonal
-        if (allEqual([boardArray[0][0], boardArray[1][1], boardArray[2][2]])) winner = true;
-        if (allEqual([boardArray[0][2], boardArray[1][1], boardArray[2][0]])) winner = true;
+        if (allEqual([array[0][0], array[1][1], array[2][2]])) winner = true;
+        if (allEqual([array[0][2], array[1][1], array[2][0]])) winner = true;
 
         return winner;
     }
 
-    function getBoard() {
-        return boardArray;
+    function playRound(x, y) {
+        gameBoard.addMark(x, y, currentMark);
+        changeMark()
+    }
+
+    function getMark() {
+        return currentMark;
+    }
+
+    function changeMark() {
+        (currentMark == 'x') ? currentMark = 'o' : currentMark = 'x';
     }
 
     return {
-        getBoard,
         checkForWinner,
+        getMark,
+        changeMark,
+        playRound,
     }
 })()
 
-console.log(gameBoard.checkForWinner());
+gameController.playRound(1, 0)
+gameController.playRound(0, 0)
+gameController.playRound(1, 1)
+gameController.playRound(0, 1)
+console.log(gameBoard.getBoard());
+console.log(gameController.checkForWinner(gameBoard.getBoard()));
